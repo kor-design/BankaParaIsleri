1a9a2b92ca99b21da791c020bd1e9280720299db
omer kor <omerkorr@gmail.com>
2020-02-14 09:36:01 +0300

    last

--- BankaParaYatirCekSorgula/BankaParaYatirCekSorgula/Program.cs
+++ BankaParaYatirCekSorgula/BankaParaYatirCekSorgula/Program.cs
@@ -6,10 +6,61 @@ using System.Threading.Tasks;
 
 namespace BankaParaYatirCekSorgula
 {
+    class BankaIsleri
+    {
+        private int bakiye = 1000000;
+        public void ParaYatir (int miktar)
+        {
+            bakiye += miktar;
+        }
+        public void ParaCek (int miktar)
+        {
+            bakiye -= miktar;
+        }
+        public int BakiyeSorgula()
+        {
+            return bakiye;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            BASADON:
+            BankaIsleri b1 = new BankaIsleri();
+            Console.WriteLine("Yapmak istediğiniz işlemi seçiniz" +
+                "\n1-ParaYatır" +
+                "\n2-ParaÇek" +
+                "\n3-BakiyeSor" +
+                "\n4-ÇıkışYap");
+            char secim = Convert.ToChar(Console.ReadLine());
+            if (secim=='3')
+            {
+                Console.WriteLine("Banka hesabınızda" + " " + b1.BakiyeSorgula() + " " + "TL vardır.");
+                goto BASADON;            
+            }
+            if (secim=='1')
+            {
+                Console.WriteLine("Ne kadar yatıracaksınız");
+                int yatirilan = int.Parse(Console.ReadLine());
+                b1.ParaYatir(yatirilan);
+                Console.WriteLine("Banka hesabınızda" + " " + b1.BakiyeSorgula() + " " + "TL vardır.");
+                goto BASADON;
+            }
+            if (secim=='2')
+            {
+                Console.WriteLine("Ne kadar çekeceksiniz");
+                int cekilen = int.Parse(Console.ReadLine());
+                b1.ParaCek(cekilen);
+                Console.WriteLine("Banka hesabınızda" + " " + b1.BakiyeSorgula() + " " + "TL vardır.");
+                goto BASADON;
+            }
+            if (secim=='4')
+            {
+                Environment.Exit(0);
+            }
+            
+            Console.Read();
         }
     }
 }
